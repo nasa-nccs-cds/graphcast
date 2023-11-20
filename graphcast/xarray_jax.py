ad89384e1d86854c38fdddebf2d6446c5d1513d4
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-11-20 14:53:37 -0700

    Add diagnostics.

--- graphcast/xarray_jax.py
+++ graphcast/xarray_jax.py
@@ -117,7 +117,7 @@ def wrap(value):
 def Variable(dims, data, **kwargs) -> xarray.Variable:  # pylint:disable=invalid-name
   """Like xarray.Variable, but can wrap JAX arrays."""
   wrapped_data = wrap(data)
-  print(f" ** Jax Variable-->>> dims={dims}, wrapped data shape = {wrapped_data.shape}, input: {data}")
+  print(f" ** Jax Variable-->>> dims={dims}, wrapped data shape = {wrapped_data.shape}, input type: {type(data)}")
   return xarray.Variable(dims, wrapped_data, **kwargs)
 
 
