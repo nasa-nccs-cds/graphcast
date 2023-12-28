eb8fe12a2b34797d7ddf0078a2dfe5f9fe9f1268
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2023-12-28 16:15:48 -0700

    Add logged diagnostics.

--- graphcast/graphcast.py
+++ graphcast/graphcast.py
@@ -375,7 +375,7 @@ class GraphCast(predictor_base.Predictor):
     # Run message passing in the multimesh.
     # [num_mesh_nodes, batch, latent_size]
     updated_latent_mesh_nodes: chex.Array = self._run_mesh_gnn(latent_mesh_nodes)
-    latents = xarray_jax.DataArray(data=updated_latent_mesh_nodes, dims=['grid', 'batch', 'features'])
+  #  latents = xarray_jax.DataArray(data=updated_latent_mesh_nodes, dims=['grid', 'batch', 'features'])
 
     # Transfer data frome the mesh to the grid.
     # [num_grid_nodes, batch, output_size]
@@ -385,7 +385,7 @@ class GraphCast(predictor_base.Predictor):
     # [num_grid_nodes, batch, output_size] ->
     # xarray (batch, one time step, lat, lon, level, multiple vars)
     prediction: xarray.Dataset = self._grid_node_outputs_to_prediction(output_grid_nodes, targets_template)
-    prediction['latents'] = latents
+ #   prediction['latents'] = latents
     return prediction
 
   def loss_and_predictions(  # pytype: disable=signature-mismatch  # jax-ndarray
